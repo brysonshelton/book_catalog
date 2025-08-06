b4771ae8b738a03dfd91699220416ff431c1d9e5
brysonshelton <bryson1679@gmail.com>
2025-08-05 19:45:34 -0500

    adds name to sign-up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     home_index_path
   end
+
+  # Permit additional Devise parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
 end
